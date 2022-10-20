86b5833741312494b0a405bf0b3d2500a4443296
Dmitriy Pravkin <dpravkin@yandex.ru>
2022-10-20 18:44:10 +0200

    Tiket

---
+++ Tiket.java
@@ -0,0 +1,11 @@
+public class Tiket {
+
+    public static void main(String[] args) {
+        int tiket = 3600;
+        byte x = 20;
+
+
+        System.out.println("Вам начисленно" + (tiket/x) + "милль(и)" );
+    }
+
+}
